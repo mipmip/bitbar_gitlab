aa3458bfa017d432ea168cb33da583467516fcb3
Pim Snel <pim@lingewoud.nl>
2019-02-25 23:16:38 +0100

    icon, time ago and version bump

--- bitbar_gitlab.gemspec
+++ bitbar_gitlab.gemspec
@@ -18,16 +18,16 @@ Gem::Specification.new do |spec|
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
-  if spec.respond_to?(:metadata)
-#    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-
-    spec.metadata["homepage_uri"] = spec.homepage
-    spec.metadata["source_code_uri"] = "https://github.com/mipmip/bitbar_gitlab"
-    spec.metadata["changelog_uri"] = "https://github.com/mipmip/mipmip-bitbar_gitlab/blob/master/CHANGELOG.md"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
-  end
+#  if spec.respond_to?(:metadata)
+##    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+#
+#    spec.metadata["homepage_uri"] = spec.homepage
+#    spec.metadata["source_code_uri"] = "https://github.com/mipmip/bitbar_gitlab"
+#    spec.metadata["changelog_uri"] = "https://github.com/mipmip/mipmip-bitbar_gitlab/blob/master/CHANGELOG.md"
+#  else
+#    raise "RubyGems 2.0 or newer is required to protect against " \
+#      "public gem pushes."
+#  end
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -43,4 +43,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "minitest", "~> 5.0"
 
   spec.add_runtime_dependency "gitlab", "~> 4.5"
+  spec.add_runtime_dependency "relative_time"
 end
--- lib/bitbar_gitlab/gitlab_bitbar_lib_menu_utils.rb
+++ lib/bitbar_gitlab/gitlab_bitbar_lib_menu_utils.rb
@@ -1,3 +1,5 @@
+require 'relative_time'
+
 def shellwrap
   "#{$conf.get_key :exe_util_dir}/shellwrap.sh"
 end
@@ -42,15 +44,48 @@ def project_menu pr, level, in_focus = false
   end
 end
 
+def pipeline_color status
+  if status == 'failed'
+    'color=red'
+  elsif status == 'success'
+    'color=green'
+  elsif status == 'running'
+    'color=blue'
+  else
+    'color=black'
+  end
+end
+
+def pipeline_text status
+  if status == 'success'
+    'passed'
+  elsif status == 'running'
+    'is running'
+  else
+    status
+  end
+end
+
 def pipeline_focus_menu pr, level, in_focus = false
 
+  require "pp"
     data = []
     labels = ['id','status','web_url']
     $gitlab.pipelines(pr.to_hash['id'].to_s,{per_page:3, page:1, state: 'running'}).collect do |iss|
 
+      more = $gitlab.pipeline(pr.to_hash['id'].to_s,iss.to_hash['id'] )
+
+      #PP.pp (Time.now - Time.parse(more.to_hash['finished_at'])).to_i
+
       d = []
       labels.each do |l|
         d << iss.to_hash[l].to_s
+      end
+
+      if more.to_hash['finished_at']
+        d << RelativeTime.in_words(Time.now - (Time.now - Time.parse(more.to_hash['finished_at'])).to_i)
+      else
+        d << ''
       end
 
       data << d
@@ -61,11 +96,11 @@ def pipeline_focus_menu pr, level, in_focus = false
     data.each do |d|
       if i == 0
         if $conf.get_key(:last_job_id) == d[0] and $conf.get_key(:last_job_status) != d[1]
-            if d[1] == 'success'
-              notify "-title 'GitLab Pipeline Passed' -message '#{d[0]} passed' -open '#{d[2]}'"
-            elsif d[1] == 'failed'
-              notify "-title 'GitLab Pipeline Failed' -message '#{d[0]} failed' -open '#{d[2]}'"
-            end
+          if d[1] == 'success'
+            notify "-title 'GitLab Pipeline Passed' -message '#{d[0]} passed' -open '#{d[2]}'"
+          elsif d[1] == 'failed'
+            notify "-title 'GitLab Pipeline Failed' -message '#{d[0]} failed' -open '#{d[2]}'"
+          end
         end
 
         $conf.set_key :last_job_id, d[0]
@@ -75,21 +110,20 @@ def pipeline_focus_menu pr, level, in_focus = false
 
       i+=1
 
-      puts "#{indent level}#{d[0]} #{d[1]} | href=#{d[2]}"
+      puts "#{indent level}job #{d[0]} #{pipeline_text d[1]} #{d[3]}| href=#{d[2]} #{pipeline_color d[1]}"
     end
 end
 
 def pipeline_menu pr, level, in_focus = false
 
+    project_info = $gitlab.project(pr.to_hash['id'].to_s)
+    return unless project_info.to_hash['jobs_enabled']
+
     data = []
-    labels = ['id', 'status','web_url']
+
+
     $gitlab.pipelines(pr.to_hash['id'].to_s,{per_page:3, page:1, state: 'running'}).collect do |iss|
-
       d = []
-      labels.each do |l|
-        d << iss.to_hash[l].to_s
-      end
-
       data << d
     end
 
@@ -105,7 +139,7 @@ def pipeline_menu pr, level, in_focus = false
     data.each do |d|
       i+=1
 
-      puts "#{indent level}#{d[0]} #{d[1]} | href=#{d[2]}"
+      puts "#{indent level}#{d[0]} #{d[1]} | href=#{d[2]} #{pipeline_color d[1]}"
     end
 end
 
